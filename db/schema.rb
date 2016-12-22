e3d9088b9efd8086b5996116c2c5944b1032031d
byronbenjamin <byrongage@yahoo.com>
2016-12-22 17:38:18 -0600

    paperclip gme and uploading pics

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161222073841) do
+ActiveRecord::Schema.define(version: 20161222232540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,9 +19,13 @@ ActiveRecord::Schema.define(version: 20161222073841) do
   create_table "pics", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "pics", ["user_id"], name: "index_pics_on_user_id", using: :btree
